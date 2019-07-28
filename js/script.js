0b7ce5c5881c53122875adf74cdf3b719f929b78
elrumo <elrumo97@me.com>
2019-07-28 16:55:17 +0100

    Seconds handle fixed

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ function clock(){
   var time=[date.getHours(), date.getMinutes(), date.getSeconds()];
   var clockDivs=[document.getElementById("hour"), document.getElementById("min"),document.getElementById("sec")];
 
-  console.log("hey");
+  // console.log("test");
 
   var hour=time[1]/2+time[0]*30;
 
@@ -22,41 +22,3 @@ function clock(){
 window.onload = function(){
   rClock();
 }
-
-// function localTime() {
-//
-//   //Get local time
-//   var date = new Date;
-//   var seconds = date.getSeconds();
-//   var minutes = date.getMinutes();
-//   var hours = date.getHours();
-//
-//   var hands = [
-//       {
-//         hand: 'hours',
-//         angle: (hours * 30) + (minutes / 2)
-//       },
-//       {
-//         hand: 'minutes',
-//         angle: (minutes * 6)
-//       },
-//       {
-//         hand: 'seconds',
-//         angle: (seconds * 6)
-//       }
-//     ];
-//     // Loop through each of these hands to set their angle
-//     for (var j = 0; j < hands.length; j++) {
-//       var elements = document.querySelectorAll('.' + hands[j].hand);
-//       for (var k = 0; k < elements.length; k++) {
-//           elements[k].style.webkitTransform = 'rotateZ('+ hands[j].angle +'deg)';
-//           elements[k].style.transform = 'rotateZ('+ hands[j].angle +'deg)';
-//           // If this is a minute hand, note the seconds position (to calculate minute position later)
-//           if (hands[j].hand === 'minutes') {
-//             elements[k].parentNode.setAttribute('data-second-angle', hands[j + 1].angle);
-//           }
-//       }
-//     }
-//   }
-//
-//   localTime()
